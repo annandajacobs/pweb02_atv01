bf639d7a17165063b14b32eed06baba1cfce18b5
annandajacobs <annandajacobs@gmail.com>
2025-10-31 09:39:20 -0300

    primeiro commit

---
+++ app.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.set('view engine', 'ejs');
+app.use(express.static('public'));
+
+let contador = 0;
+
+app.get('/', (req, res) => {
+    contador++;
+    res.render('index',  {title: 'Página inicial da aplicação', contador});
+})
+
+app.get('/sobre', (req, res) => {
+    contador++;
+  res.render('index', { title: 'Sobre nós', contador});
+});
+
+app.listen(port, () => {
+    console.log(`Servidor rodando em: http://localhost:${port}`);
+})
